f878ebad3ad58f851cb2af3951f63c781b2ed592
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-02-14 15:59:24 +0530

    Create alphabet.cpp

---
+++ alphabet.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char a;
+  cout<<"Enter the input";
+  cin>>a;
+  if((a >= 'a'&& a<='z') || (a>='A'&& a<='Z'))
+  {
+      cout<<"it is alphabet";
+  }
+  else if (a>='0'&&a<='9')
+  {
+      cout<<"It is number";
+  }
+  else
+  {
+      cout<<" It is special character";
+  }
+}
+
+  
+  
+
