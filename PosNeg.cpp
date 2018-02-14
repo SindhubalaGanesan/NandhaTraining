5d4ccc3979cb6629a5c15c56af458de341540584
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-02-14 14:57:05 +0530

    Create PosNeg.cpp

---
+++ PosNeg.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  int i;
+  cout<<"Enter your number";
+  cin>>i;
+  if(i>=1&&i<=100000)
+  {
+  if(i>0)
+  {
+      cout<<"It is positive";
+}
+else
+{
+    cout<<"It is negative";
+}
+}
+else
+{
+    cout<<"It is greater than 100000& all are will be positive";
+}
+
+
+    return 0;
+}
+
