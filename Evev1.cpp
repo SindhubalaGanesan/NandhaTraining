a04491ab1b89e2c73f36cc89fe77977d84d28c95
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-02-15 15:15:20 +0530

    Create Evev1.cpp

---
+++ Evev1.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+class Even
+{
+    private:
+    int a,b,i;
+    public:
+    void get()
+    {
+        cout<<"Enter the number";
+        cin>>a>>b;
+        for(i=a;i<=b;i++)
+        { 
+           
+            if(a%2==0)
+            {
+                cout<<a;
+            }
+             a=a+1;
+        }
+    }
+
+};
+
+int main()
+{
+ Even e;
+ e.get();
+  
+}
