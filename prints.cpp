a2a5037bc12b30c9a4f6088463c93fcd7c7c9c68
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-02-15 12:16:21 +0530

    Create prints.cpp

---
+++ prints.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+class Print5
+{
+    private:
+    int n,i,a[10000];
+    public:
+    void get()
+    {
+        cout<<"enter the value of n";
+        cin>>n;
+        for(i=1;i<=5;i++)
+        {
+            a[i]=i*n;
+            cout<<a[i]<<"\t";
+        }
+    }
+};
+
+int main()
+{
+  Print5 p;
+ p.get();
+
+ }
