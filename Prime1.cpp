bda806617c864a7d27eeba143f44d5caf80d17a8
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-02-16 17:31:36 +0530

    Create Prime1.cpp

---
+++ Prime1.cpp
@@ -0,0 +1,43 @@
+
+
+#include <iostream>
+
+using namespace std;
+class Prime
+{
+    private:
+    int i,a,b,count=0,j;
+    public:
+    void get()
+    {
+        cout<<"enter your number";
+        cin>>a;
+        cin>>b;
+        for(i=a;i<=b;i++)
+        {
+            count=0;
+            for(j=2;j<=i/2;j++)
+            {
+                if(i%j==0)
+                {
+                    count++;
+                    break;
+                    
+                }
+            }
+            if(count==0)
+            
+                cout<<"The output is"<<i<<"\t";
+            
+        }
+    
+    }
+};
+
+int main()
+{
+    Prime p;
+    p.get();
+
+    return 0;
+}
