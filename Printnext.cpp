43d810223e50f611b41c83b9f261caab2fd5fc67
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-02-20 21:44:42 +0530

    Update Printnext.cpp

--- Printnext.cpp
+++ Printnext.cpp
@@ -10,8 +10,15 @@ class Sum
   {
       cout<<"Enter the number"<<endl;
       cin>>n;
+      if(n<10000000)
+      {
       c=n+1;
       cout<<"Output is:"<<c<<endl;
+      }
+      else
+      {
+          cout<<"Enter the number upto 10000000"<<endl;
+      }
   }
 };
 
