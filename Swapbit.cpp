23f060f23111d47653825567a02fb106110f10bc
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-02-23 17:08:24 +0530

    Create Swapbit.cpp

---
+++ Swapbit.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+using namespace std;
+class Swap
+{
+    private:
+    int a,b;
+    public:
+    void display()
+    {    int temp;
+        cout<<"Enter the number:"<<endl;
+        cin>>a>>b;
+      
+        if(a<100000&&b<100000)
+        {
+       a=a^b;
+       b=a^b;
+       a=a^b;
+        cout<<"After swapping:"<<endl;
+         cout<<"a="<<a<<"b="<<b<<endl;
+        }
+        else
+        {
+            cout<<"Enter the input upto 100000"<<endl;
+        }
+    }
+};
+
+int main()
+{
+    Swap s;
+    s.display();
+
+    return 0;
+}
