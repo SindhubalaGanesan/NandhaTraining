b3f946fb8828ae0768611fa29473c0cd291d0edb
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-03-03 20:20:27 +0530

    Create wholenum.c

---
+++ wholenum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float a;
+int b;
+clrscr();
+printf("Enter the number");
+scanf("%f",&a);
+b=ceil(a);
+printf("%d",b);
+getch();
+}
