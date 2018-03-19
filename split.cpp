f31e1c3d123016824b565185cc998f3bb695e0a5
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-03-19 09:26:42 +0530

    Create split.cpp

---
+++ split.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Example
+{
+  private:
+  int a,i,c=0,rem[100];
+  public:
+  void display()
+  {
+      cout<<"Enter the number"<<endl;
+      cin>>a;
+      while(a>0)
+      {
+          rem[c]=a%10;
+          a=a/10;
+          c++;
+      }
+      for(c=c-1;c>=0;c--)
+      {
+          cout<<"\t"<<rem[c];
+      }
+  }
+};
+
+int main()
+{
+    Example e;
+    e.display();
+
+    return 0;
+}
