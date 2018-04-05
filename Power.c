aa40e8ac099bb4b20238b3bef2435236e4d2aab2
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-04-05 14:00:31 +0530

    Create Power.c

---
+++ Power.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the two input");
+    scanf("%d%d",&a,&b);
+    c=pow(a,b);
+    printf("The resull is:%d",c);
+}
