3680487f91bbdcf996c266c441402655981f9e52
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-05-21 10:23:57 +0530

    Update Fact6.cpp

--- Fact6.cpp
+++ Fact6.cpp
@@ -10,6 +10,8 @@ class Fact
         cout<<"Enter the number"<<endl;
         cin>>n;
         cout<<"The answer is:"<<endl;
+        if(n<1000)
+        {
         for(i=1;i<=n;i++)
         {
             if(n%i==0)
@@ -17,6 +19,12 @@ class Fact
                 cout<<i<<"\t";
             }
         }
+        }
+        else
+        {
+            cout<<"Enter only less than 1000"<<endl;
+        }
+        
     }
 };
 int main()
