2287c733b3610676cccc68fee8596e83d6775d9a
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-05-21 16:27:41 +0530

    Update Search1.cpp

--- Search1.cpp
+++ Search1.cpp
@@ -12,6 +12,8 @@ class Example
         cout<<"Enter the k value"<<endl;
         cin>>k;
         int a[n];
+        if(n<1000000000000000)
+        {
         for(i=0;i<n;i++)
         {
             cin>>a[i];
@@ -31,6 +33,11 @@ class Example
         else
         {
             cout<<"No";
+        }
+        }
+        else
+        {
+            cout<<"Enter only less than 1000000000000000"<<endl;
         }
         
     }
