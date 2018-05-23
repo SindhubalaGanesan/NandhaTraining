3a2b1df03963bbb0a89842c43b1f79b25b1215a7
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-05-23 16:16:55 +0530

    Update deletek.cpp

--- deletek.cpp
+++ deletek.cpp
@@ -1,1 +1,34 @@
 
+#include<iostream>
+using namespace std;
+class Example
+{
+  private:
+  int n,i,k;
+  public:
+  void display()
+  {
+      cout<<"enter the value of k and n:"<<endl;
+      cin>>n;
+      int a[n];
+      cin>>k;
+      for(i=0;i<n;i++)
+      {
+         cin>>a[i];
+      }
+      for(i=0;i<n-k;i++)
+      {
+          cout<<a[i]<<"\t";
+      }
+          
+      
+  }
+       
+};
+int main()
+{
+    Example e;
+    e.display();
+     return 0;
+}
+
