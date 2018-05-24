7d325082d89c29e1b581fb1a24d91ad18e53e35f
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-05-24 14:25:38 +0530

    Create arrayindex.cpp

---
+++ arrayindex.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+class Example
+{
+    private:
+    int n,count=0,i;
+    public:
+    int  display()
+    {
+    
+        cout<<"enter the number:";
+        cin>>n;
+        int a[n],b[n];
+        for(i=0;i<n;i++)
+        {
+             cin>>a[i];
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==i)
+            {
+                cout<<a[i]<<"\t";
+                count++;
+                
+            }
+            }
+      
+       if(count==0)
+       {
+           cout<<"-1";
+       }
+    }
+    
+        
+};
+
+int main()
+{
+    Example e;
+    e.display();
+
+    return 0;
+}
