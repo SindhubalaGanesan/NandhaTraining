047dd75642217937ea2d8b54ba484658338623b2
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-05-24 09:29:08 +0530

    Create Negativenumber.cpp

---
+++ Negativenumber.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+class Example
+{
+  private:
+ int n,i,flag;
+ public:
+ void display()
+ {
+     cout<<"enter the number:"<<endl;
+     cin>>n;
+     int a[n];
+     for(i=0;i<n;i++)
+     {
+         cin>>a[i];
+     }
+     for(i=0;i<n;i++)
+     {
+         if(a[i]<=0)
+         {
+             cout<<a[i]<<"\t";
+         }
+        
+     }
+     
+ }
+};
+
+int main()
+{
+ Example e;
+ e.display();
+
+    return 0;
+}
