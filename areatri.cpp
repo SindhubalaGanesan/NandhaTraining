985608c6217fc24b798a3e59e3dee7db3369993f
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-05-24 09:07:32 +0530

    Update areatri.cpp

--- areatri.cpp
+++ areatri.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+using namespace std;
+class Example
+{
+  private:
+  int b,h;
+  public:
+  void display()
+  {
+      cout<<"Enter the base and height:";
+      cin>>b>>h;
+      int c=0.5*b*h;
+      cout<<"The area of triangle:"<<c<<endl;
+  }
+};
+
+int main()
+{
+ Example e;
+ e.display();
+
+    return 0;
+}
