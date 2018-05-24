5335555cd69af6945516f35bd080ba85ae772c3e
SindhubalaGanesan <36263675+SindhubalaGanesan@users.noreply.github.com>
2018-05-24 16:24:52 +0530

    Update reveplus.c

--- reveplus.c
+++ reveplus.c
@@ -6,6 +6,8 @@ int b,re,sum1=0;
 clrscr();
 printf("Enter the number");
 scanf("%d",&b);
+if(b<10000)
+{
 while(b!=0)
 {
 re=b%10;
@@ -13,5 +15,10 @@ sum1=sum1+(re*re);
 b=b/10;
 }
 printf("The result is=%d",sum1);
+}
+else
+{
+    printf("enter only less than 10000");
+}
 getch();
 }
